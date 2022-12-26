cae6190b5d95ebbae1cc067685a4e48f0cf22652
Idan Liberman <idanlib@gmail.com>
2022-12-26 16:00:03 +0200

    solution committed

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+const express = require('express');
+const app = express();
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
@@ -24,7 +24,19 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+//Header information parser
+app.get('/api/whoami', (req, res) => {
+  // console.log(req.ip);
+  // console.log(req.headers["accept-language"]);
+  // console.log(req.headers["user-agent"]);
+
+  return res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
+
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
